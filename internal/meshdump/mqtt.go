129c0d73fdab081ec357d29c61c8efb0d75e3354
Niccolò Malenotti <malenottinicco@gmail.com>
2025-07-15 00:25:10 +0200

    Replace stub MQTT with real library

--- internal/meshdump/mqtt.go
+++ internal/meshdump/mqtt.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"log"
 
-	mqtt "meshdump/internal/stubmqtt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 // StartMQTT connects to the given broker and subscribes to the provided topic.
