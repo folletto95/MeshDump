3ef00b4a0a50a39a5446e2d941d8b76794d24a68
Niccolò Malenotti <malenottinicco@gmail.com>
2025-07-16 00:47:00 +0200

    Add multi-target build support

--- build.sh
+++ build.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 set -e
 
-echo "Building Windows binary using Docker..."
-docker run --rm -v "$PWD":/src -w /src golang:1.23 \
-    sh -c 'go mod tidy && \
-    GOOS=windows GOARCH=amd64 go build -buildvcs=false -o MeshDump.exe ./cmd/meshdump'
+OS=${1:-linux}
+ARCH=${2:-amd64}
 
-echo "Binary available at MeshDump.exe"
+build() {
+    os=$1
+    arch=$2
+    output="MeshDump"
+    if [ "$os" = "windows" ]; then
+        output="MeshDump.exe"
+    fi
+
+    echo "Building $os/$arch binary using Docker..."
+    docker run --rm -v "$PWD":/src -w /src golang:1.23 \
+        sh -c "go mod tidy && \
+        GOOS=$os GOARCH=$arch go build -buildvcs=false -o $output ./cmd/meshdump"
+
+    echo "Binary available at $output"
+}
+
+if [ "$OS" = "all" ]; then
+    for os in linux windows; do
+        build "$os" "$ARCH"
+    done
+else
+    build "$OS" "$ARCH"
+fi
